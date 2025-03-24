c12c1d58ed8d62f5a1763050ca150fd02247b98b
Braja Gopal Das <Dbraja@pkrmbendc.com>
2025-03-24 18:57:01 +0530

    worked on stringsm, arrays and functions

---
+++ output.js
@@ -0,0 +1,62 @@
+// Exp: 1
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// EXP 2
+// console.log(
+//   "Hello \n\
+// How are \n\
+// you"
+// );
+
+// In template literals
+// console.log(`Hello 
+// How are 
+// you`);
+
+// exp3
+// const inputYear = "1991";
+// console.log(Number(inputYear) + 18);
+// console.log(inputYear + 18);
+// console.log(inputYear);
+// console.log(typeof inputYear);
+// console.log(typeof NaN); // NaN means an invalid number its a number some how that why typeof NaN is number
+// console.log(String(23));
+// console.log("23" - "10" - 3);
+// console.log("23" - "10" - "3");
+// console.log("23" * "2");
+// console.log("23" / "2");
+
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+// console.log(2 + 3 + 4 + "5");
+// console.log("10" - "4" - "3" - 2 + "5");
+
+
+// exp
+// 5 falsy values: 0, '', undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Gopal'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+
+const age = 18; // Here "=" is assignment operator
+// "===" is known as strict equality operator.
+// It is strict because does not perform any type coersion
+// So it only returns true when both values exactly the same
+// here "===" is comparision operator
+console.log(age === 18);    // true
+console.log('18' === 18);    // false
+
+ 
+
+// "==" is loose equality operator
+// It does type coersion
+// here "==" is comparision operator
+console.log('18' == 18);    // true
+
